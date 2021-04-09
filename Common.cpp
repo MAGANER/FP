1f7bf8efc6479d641f750692d5d5aa94ba7b6998
MAGANER <maganer.krik@mail.ru>
2021-04-09 17:19:35 +0300

    modified arguments parser based on custom keys/special keys of different modules

--- Common.cpp
+++ Common.cpp
@@ -1,19 +1,46 @@
 #include"Common.h"
-pair<char, string> Common::parse_argument(string arg)
+pair<char, string> Common::inner::parse_argument(const string& arg,
+												 const string* keys,
+												 int keys_size,
+												 const string* special_keys,
+												 int spec_keys_size)
 {
-	bool special_key = arg != "-c";
+	auto is_inside = [](const string& val, const string* arr, int size)
+	{
+		for (int i = 0; i < size; ++i)
+		{
+			if (val == arr[i]) return true;
+		}
+		return false;
+	};
 
-	if (arg.size() < 4 && special_key) Common::kill_app("argument is too short!");
+	bool special_key = is_inside(arg, special_keys, spec_keys_size);
+	bool regular_key = is_inside(arg, keys, keys_size);
 
-	string  keys = "-w-p-c-f";
-	if (keys.find(arg.substr(0, 2)) == string::npos)
+	if(arg.size() < 4 && !special_key)  Common::kill_app("argument is too short! >> "+arg);
+	if (regular_key && !special_key)
 		Common::kill_app("no such key:" + arg.substr(0, 2));
-	if (arg.find('=') == string::npos && special_key)
+	if (arg.find('=') == string::npos && !special_key)
 		Common::kill_app("incorrect argument:" + arg);
+
 
 	char key = arg[1];
 	string val = Common::get_substr(arg, 3, arg.size());
 	return make_pair(key, val);
+}
+map<char, string> Common::parse_arguments(const vector<string>& args,
+										  const string* keys,
+										  int keys_size,
+										  const string* special_keys,
+										  int spec_keys_size)
+{
+	map<char, string> options;
+	for (auto& _arg : args)
+	{
+		auto option = Common::inner::parse_argument(_arg, keys,keys_size, special_keys,spec_keys_size);
+		options[option.first] = option.second;
+	}
+	return options;
 }
 void Common::kill_app(string err)
 {
--- Common.h
+++ Common.h
@@ -2,12 +2,27 @@
 #include<string>
 #include<utility>
 #include<iostream>
+#include<map>
+#include<vector>
 
 using namespace std;
 namespace Common
 {
 	void kill_app(string err);
-	pair<char, string> parse_argument(string arg);
 	bool does_match_with_predicat(string predicat, string value);
 	string get_substr(string str, int begin, int end);
+
+	map<char, string> parse_arguments(const vector<string>& args, 
+									  const string* keys,
+									  int keys_size,
+								      const string* special_keys,
+									  int spec_keys_size);
+	namespace inner
+	{
+		pair<char, string> parse_argument(const string& arg,
+										  const string* keys,
+										  int keys_size,
+										  const string* special_keys,
+										  int spec_keys_size);
+	};
 };
