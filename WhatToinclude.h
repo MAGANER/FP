13ae7a9913af81caf7e97f5b7b052b4719212e86
MAGANER <maganer.krik@mail.ru>
2021-04-09 14:25:16 +0300

    this header defines which modules will be compiled into the programm

---
+++ WhatToinclude.h
@@ -0,0 +1,11 @@
+/*
+	Here you can choose what should be included
+	in your version of application. 
+	If you bound 1 to speficic module,
+	than it will be used.
+*/
+
+#pragma once
+#define INCLUDE_COUNTER 0 //don't change it! it counts number of included modules
+
+#define COPY 1
